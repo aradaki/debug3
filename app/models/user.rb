4e21afd6ea51626f8e172863aad2a308e0291e4b
EC2 Default User <ec2-user@ip-172-31-87-212.ec2.internal>
2021-11-04 10:30:08 +0000

    finish

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   attachment :profile_image, destroy: false
 
   has_many :books
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :books
   devise_for :users
   root 'homes#top'
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   resources :users,only: [:show,:index,:edit,:update]
   get 'home/about' => 'homes#about'
 end
